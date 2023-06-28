7f0434f6160a7759b4b485101f7db643abf50fed
tenzin choenyi <fnu.tenzinchoenyi@mail.citytech.cuny.edu>
2023-06-27 23:47:16 -0400

    first commit

---
+++ src/test/java/com/render/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.render.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
